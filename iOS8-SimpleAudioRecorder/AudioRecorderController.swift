e3ec663b30c13afe48f8ae081269bbd951256bb7
Michael Redig <mredig@gmail.com>
2019-10-01 11:23:23 -0500

    imported provided assets

---
+++ iOS8-SimpleAudioRecorder/AudioRecorderController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  AudioRecorder
+//
+//  Created by Paul Solt on 10/1/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecorderController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+
+	private lazy var timeFormatter: DateComponentsFormatter = {
+		let formatting = DateComponentsFormatter()
+		formatting.unitsStyle = .positional // 00:00
+		formatting.zeroFormattingBehavior = .pad
+		formatting.allowedUnits = [.minute, .second]
+		return formatting
+	}()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize,
+                                                          weight: .regular)
+        timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
+                                                                   weight: .regular)
+	}
+
+
+    @IBAction func playButtonPressed(_ sender: Any) {
+
+	}
+    
+    @IBAction func recordButtonPressed(_ sender: Any) {
+    
+    }
+}
+
